650d18ff932027ce6693c4168bb66aab65b8bb7d
Adam Szentirmai-Schon <adamsan3@gmail.com>
2022-05-24 08:46:48 +0200

    initial commit

---
+++ src/main/java/com/example/bookapimanytomany/BookApiManyToManyApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookapimanytomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApiManyToManyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookApiManyToManyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookapimanytomany/BookApiManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookapimanytomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiManyToManyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
