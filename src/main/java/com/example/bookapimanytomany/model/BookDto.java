b73f7159a55f5ba7ef37974848e9a3b0e9cc6f29
szemanlevi <szemanlevente@gmail.com>
2023-03-27 10:24:14 +0200

    BookDTO corrected

--- src/main/java/com/example/bookapimanytomany/model/BookDto.java
+++ src/main/java/com/example/bookapimanytomany/model/BookDto.java
@@ -1,44 +1,16 @@
 package com.example.bookapimanytomany.model;
 
+import lombok.*;
+
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class BookDto {
     private String title;
 
     private List<Long> authorIdList;
-
-    public List<Long> getAuthorIdList() {
-        return authorIdList;
-    }
-
-    public void setAuthorIdList(List<Long> authorIdList) {
-        this.authorIdList = authorIdList;
-    }
-
-    public BookDto(String title) {
-        this.title = title;
-    }
-
-    public BookDto(String title, List<Long> authorIdList) {
-        this.title = title;
-        this.authorIdList = authorIdList;
-    }
-
-    public BookDto() {
-    }
-
-    @Override
-    public String toString() {
-        return "BookDto{" +
-                "title='" + title + '\'' +
-                '}';
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
